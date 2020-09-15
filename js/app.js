947c35fc486dc03ee60b462bad4b63fd7fd4fef2
Vasiliy <vasiliyrodionov@gmail.com>
2020-09-15 20:11:40 +0300

    0.8

--- js/app.js
+++ js/app.js
@@ -2,4 +2,6 @@ $( ".nav-btn" ).click(function() {
   $( ".nav-menu" ).toggleClass( "active" );
   $( "body" ).toggleClass( "lock" );
   $( ".logo-menu" ).toggleClass( "active" );
+  $(".nav-btn").toggleClass("active");
 });
+
